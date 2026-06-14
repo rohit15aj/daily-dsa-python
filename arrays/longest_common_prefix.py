c2a45232bb16930481db59e15166e0fc290dbe52
ROHIT PRAKASH <rohit_ua2504aih185@iitp.ac.in>
2026-06-14 20:22:54 +0530

    Create longest_common_prefix.py

---
+++ arrays/longest_common_prefix.py
@@ -0,0 +1,14 @@
+lass Solution:
+    def longestCommonPrefix(self, strs):
+        
+        prefix = strs[0]
+
+        for word in strs[1:]:
+
+            while not word.startswith(prefix):
+                prefix = prefix[:-1]
+
+                if not prefix:
+                    return ""
+
+        return prefix
