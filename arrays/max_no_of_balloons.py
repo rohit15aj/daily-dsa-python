231e8d8f783d207b663dc58c43620dd2ff297f3d
ROHIT PRAKASH <rohit_ua2504aih185@iitp.ac.in>
2026-06-22 10:37:12 +0530

    Create max_no_of_balloons.py

---
+++ arrays/max_no_of_balloons.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution:
+    def maxNumberOfBalloons(self, text):
+
+        freq = Counter(text)
+
+        return min(
+            freq['b'],
+            freq['a'],
+            freq['l'] // 2,
+            freq['o'] // 2,
+            freq['n']
+        )
